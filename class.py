6acc82d88fcfc8029180f458c7adf586ef5761c4
Masharipov Mashribjon <rghbergberbrgerger@gmail.com>
2026-01-19 18:00:25 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,285 @@
+# # 1-masala
+# class talaba:
+#     def __init__(self,ism,yoshi,kurs):
+#         self.ism = ism
+#         self.yoshi = yoshi
+#         self.kursi = kurs
+#
+# Ali = talaba("Ali","20 yosh", "2-kurs")
+# Vali = talaba("Vali","21 yosh", "3-kurs")
+# Hasan = talaba("Hasan","19 yosh", "1-kurs")
+# Husan = talaba("Husan","22 yosh", "4-kurs")
+# Olim = talaba("Olim","20 yosh", "2-kurs")
+# Salimbek = talaba("Salimbek","20 yosh", "3-kurs")
+# print("_"*20)
+# print(f"{Ali.ism} , {Ali.yoshi} , {Ali.kursi}")
+# print(f"{Vali.ism} , {Vali.yoshi} , {Vali.kursi}")
+# print(f"{Hasan.ism} , {Hasan.yoshi} , {Hasan.kursi}")
+# print(f"{Husan.ism} , {Husan.yoshi}, {Husan.kursi}")
+# print(f"{Salimbek.ism} , {Salimbek.yoshi} , {Salimbek.kursi}")
+
+# # 2-masala
+# class Kitob:
+#     def __init__(self, nomi, muallifi, narxi):
+#         self.nomi = nomi
+#         self.muallifi = muallifi
+#         self.narxi = narxi
+#
+# kitob1 = Kitob("Python dasturlash", "Ali", 50000)
+# kitob2 = Kitob("Algoritmlar", "Vali", 75000)
+# kitob3 = Kitob("Ma'lumotlar strukturalari", "Sardor", 60000)
+# kitob4 = Kitob("Web dasturlash", "Hasan", 80000)
+# kitob5 = Kitob("Ma'shuniyat", "Olim", 45000)
+# print("_"*20)
+# print(f"{kitob1.nomi} , {kitob1.muallifi} , {kitob1.narxi}")
+# print(f"{kitob2.nomi} , {kitob2.muallifi} , {kitob2.narxi}")
+# print(f"{kitob3.nomi} , {kitob3.muallifi} , {kitob3.narxi}")
+# print(f"{kitob4.nomi} , {kitob4.muallifi} , {kitob4.narxi}")
+# print(f"{kitob5.nomi} , {kitob5.muallifi} , {kitob5.narxi}")
+
+# # 3-masala
+# class Telefon:
+#     def __init__(self,modeli,xotirasi,rangi):
+#         self.modeli = modeli
+#         self.xotirasi = xotirasi
+#         self.rangi = rangi
+#
+# iphone = Telefon("iphone 14","128 gb", "qora")
+# samsung = Telefon("samsung s23","256 gb", "oq")
+# xiaomi = Telefon("xiaomi 13","128 gb", "ko'k")
+# Huawei = Telefon("Huawei p50","512 gb", "kumush")
+# oneplus = Telefon("OnePlus 11","256", "yashil")
+# print("_"*20)
+# print(f"{iphone.modeli} , {iphone.xotirasi} , {iphone.rangi}")
+# print(f"{samsung.modeli} , {samsung.xotirasi} , {samsung.rangi}")
+# print(f"{xiaomi.modeli} , {xiaomi.xotirasi} , {xiaomi.rangi}")
+# print(f"{Huawei.modeli} , {Huawei.xotirasi}, {Huawei.rangi}")
+# print(f"{oneplus.modeli} , {oneplus.xotirasi} , {oneplus.rangi}")
+
+# # 4-masla
+# class Meva:
+#     def __init__(self, nomi, vazni, narxi):
+#         self.nomi = nomi
+#         self.vazni = vazni
+#         self.narxi = narxi
+#
+# meva1 = Meva("Olma", 1, 15000)
+# meva2 = Meva("Banan", 1.5, 20000)
+# meva3 = Meva("Uzum", 0.5, 12000)
+# meva4 = Meva("Anor", 2, 30000)
+# meva5 = Meva("Nok", 1, 10000)
+# print("_"*20)
+# print(f"{meva1.nomi} , {meva1.vazni} , {meva1.narxi}")
+# print(f"{meva2.nomi} , {meva2.vazni} , {meva2.narxi}")
+# print(f"{meva3.nomi} , {meva3.vazni} , {meva3.narxi}")
+# print(f"{meva4.nomi} , {meva4.vazni} , {meva4.narxi}")
+# print(f"{meva5.nomi} , {meva5.vazni} , {meva5.narxi}")
+
+# # 5-masala
+# class Sportchi:
+#     def __init__(self, ismi, yoshi, sport_turi):
+#         self.ismi = ismi
+#         self.yoshi = yoshi
+#         self.sport_turi = sport_turi
+#
+# sportchi1 = Sportchi("Ali", 25, "Futbol")
+# sportchi2 = Sportchi("Vali", 22, "Basketbol")
+# sportchi3 = Sportchi("Hasan", 28, "Tennis")
+# sportchi4 = Sportchi("Husan", 20, "Suzish")
+# sportchi5 = Sportchi("Olim", 24, "Voleybol")
+# print("_"*20)
+# print(f"{sportchi1.ismi} , {sportchi1.yoshi} , {sportchi1.sport_turi}")
+# print(f"{sportchi2.ismi} , {sportchi2.yoshi} , {sportchi2.sport_turi}")
+# print(f"{sportchi3.ismi} , {sportchi3.yoshi} , {sportchi3.sport_turi}")
+# print(f"{sportchi4.ismi} , {sportchi4.yoshi} , {sportchi4.sport_turi}")
+# print(f"{sportchi5.ismi} , {sportchi5.yoshi} , {sportchi5.sport_turi}")
+
+# # 6-masala
+# class Xodim:
+#     def __init__(self, ismi, lavozimi, maoshi):
+#         self.ismi = ismi
+#         self.lavozimi = lavozimi
+#         self.maoshi = maoshi
+#
+# xodim1 = Xodim("Ali", "Dasturchi", 5_000_000)
+# xodim2 = Xodim("Vali", "Dizayner", 4_000_000)
+# xodim3 = Xodim("Hasan", "Menejer", 7_000_000)
+# xodim4 = Xodim("Husan", "Tester", 3_500_000)
+# xodim5 = Xodim("Olim", "DevOps", 6_000_000)
+# print("_"*20)
+# print(f"{xodim1.ismi} , {xodim1.lavozimi} , {xodim1.maoshi}")
+# print(f"{xodim2.ismi} , {xodim2.lavozimi} , {xodim2.maoshi}")
+# print(f"{xodim3.ismi} , {xodim3.lavozimi} , {xodim3.maoshi}")
+# print(f"{xodim4.ismi} , {xodim4.lavozimi} , {xodim4.maoshi}")
+# print(f"{xodim5.ismi} , {xodim5.lavozimi} , {xodim5.maoshi}")
+
+# # 7-masala
+# class Avtomobil:
+#     def __init__(self, modeli, rangi, yili):
+#         self.modeli = modeli
+#         self.rangi = rangi
+#         self.yili = yili
+#
+# avto1 = Avtomobil("Chevrolet Malibu", "Oq", 2020)
+# avto2 = Avtomobil("Toyota Camry", "Qora", 2022)
+# avto3 = Avtomobil("Hyundai Sonata", "Kumush", 2019)
+# avto4 = Avtomobil("Kia K5", "Ko'k", 2023)
+# avto5 = Avtomobil("BMW X5", "Qizil", 2021)
+# print("_"*20)
+# print(f"{avto1.modeli} , {avto1.rangi} , {avto1.yili}")
+# print(f"{avto2.modeli} , {avto2.rangi} , {avto2.yili}")
+# print(f"{avto3.modeli} , {avto3.rangi} , {avto3.yili}")
+# print(f"{avto4.modeli} , {avto4.rangi} , {avto4.yili}")
+# print(f"{avto5.modeli} , {avto5.rangi} , {avto5.yili}")
+
+# # 8-masala
+# class Mahsulot:
+#     def __init__(self, nomi, miqdori, narxi):
+#         self.nomi = nomi
+#         self.miqdori = miqdori
+#         self.narxi = narxi
+#
+# m1 = Mahsulot("Kompyuter", 10, 8_000_000)
+# m2 = Mahsulot("Monitor", 15, 2_000_000)
+# m3 = Mahsulot("Klaviatura", 20, 200_000)
+# m4 = Mahsulot("Sichqoncha", 25, 100_000)
+# m5 = Mahsulot("Printer", 5, 3_000_000)
+# print("_"*20)
+# print(f"{avto1.nomi} , {avto1.miqdori} , {avto1.narxi}")
+# print(f"{avto2.nomi} , {avto2.miqdori} , {avto2.narxi}")
+# print(f"{avto3.nomi} , {avto3.miqdori} , {avto3.narxi}")
+# print(f"{avto4.nomi} , {avto4.miqdori} , {avto4.narxi}")
+# print(f"{avto5.nomi} , {avto5.miqdori} , {avto5.narxi}")
+
+# # 9-masala
+# class Oquvchi:
+#     def __init__(self, ismi, sinfi, ortacha_baho):
+#         self.ismi = ismi
+#         self.sinfi = sinfi
+#         self.ortacha_baho = ortacha_baho
+#
+# o1 = Oquvchi("Ali", "9-sinf", 4.5)
+# o2 = Oquvchi("Vali", "10-sinf", 4.8)
+# o3 = Oquvchi("Hasan", "9-sinf", 3.9)
+# o4 = Oquvchi("Husan", "11-sinf", 4.2)
+# o5 = Oquvchi("Olim", "10-sinf", 5.0)
+# print("_"*20)
+# print(f"{o1.ismi} , {o1.sinfi} , {o1.ortacha_baho}")
+# print(f"{o2.ismi} , {o2.sinfi} , {o2.ortacha_baho}")
+# print(f"{o3.ismi} , {o3.sinfi} , {o3.ortacha_baho}")
+# print(f"{o4.ismi} , {o4.sinfi} , {o4.ortacha_baho}")
+# print(f"{o5.ismi} , {o5.sinfi} , {o5.ortacha_baho}")
+
+# # 10-masala
+# class Uy:
+#     def __init__(self, manzili, xonalar_soni, narxi):
+#         self.manzili = manzili
+#         self.xonalar_soni = xonalar_soni
+#         self.narxi = narxi
+#
+# u1 = Uy("Chilonzor", 2, 80000)
+# u2 = Uy("Yunusobod", 3, 120000)
+# u3 = Uy("Sergeli", 1, 45000)
+# u4 = Uy("Mirzo Ulug'bek", 4, 180000)
+# u5 = Uy("Yakkasaroy", 3, 150000)
+# print("_"*20)
+# print(f"{u1.manzili} , {u1.xonalar_soni} , {u1.narxi}")
+# print(f"{u2.manzili} , {u2.xonalar_soni} , {u2.narxi}")
+# print(f"{u3.manzili} , {u3.xonalar_soni} , {u3.narxi}")
+# print(f"{u4.manzili} , {u4.xonalar_soni} , {u4.narxi}")
+# print(f"{u5.manzili} , {u5.xonalar_soni} , {u5.narxi}")
+
+# # 11-masala
+# class Film:
+#     def __init__(self, nomi, yili, reytingi):
+#         self.nomi = nomi
+#         self.yili = yili
+#         self.reytingi = reytingi
+#
+# f1 = Film("Inception", 2010, 8.8)
+# f2 = Film("The Dark Knight", 2008, 9.0)
+# f3 = Film("Interstellar", 2014, 8.6)
+# f4 = Film("Tenet", 2020, 7.4)
+# f5 = Film("Oppenheimer", 2023, 8.5)
+# print("_"*20)
+# print(f"{f1.nomi} , {f1.yili} , {f1.reytingi}")
+# print(f"{f2.nomi} , {f2.yili} , {f2.reytingi}")
+# print(f"{f3.nomi} , {f3.yili} , {f3.reytingi}")
+# print(f"{f4.nomi} , {f4.yili} , {f4.reytingi}")
+# print(f"{f5.nomi} , {f5.yili} , {f5.reytingi}")
+
+# # 12-masala
+# class Hayvon:
+#     def __init__(self, nomi, turi, yoshi):
+#         self.nomi = nomi
+#         self.turi = turi
+#         self.yoshi = yoshi
+#
+# h1 = Hayvon("Sherxon", "Sher", 8)
+# h2 = Hayvon("Fila", "Fil", 25)
+# h3 = Hayvon("Maymuncha", "Maymun", 5)
+# h4 = Hayvon("Jirafchik", "Jirafa", 12)
+# h5 = Hayvon("Zebra", "Zebra", 6)
+# print("_"*20)
+# print(f"{h1.nomi} , {h1.turi} , {h1.yoshi}")
+# print(f"{h2.nomi} , {h2.turi} , {h2.yoshi}")
+# print(f"{h3.nomi} , {h3.turi} , {h3.yoshi}")
+# print(f"{h4.nomi} , {h4.turi} , {h4.yoshi}")
+# print(f"{h5.nomi} , {h5.turi} , {h5.yoshi}")
+
+# # 13-masala
+# class Taom:
+#     def __init__(self, nomi, kategoriyasi, narxi):
+#         self.nomi = nomi
+#         self.kategoriyasi = kategoriyasi
+#         self.narxi = narxi
+#
+# t1 = Taom("Palov", "Asosiy taom", 35000)
+# t2 = Taom("Somsa", "Pishiriqlar", 8000)
+# t3 = Taom("Choy", "Ichimliklar", 5000)
+# t4 = Taom("Shashlik", "Asosiy taom", 45000)
+# t5 = Taom("Lag'mon", "Asosiy taom", 30000)
+# print("_"*20)
+# print(f"{t1.nomi} , {t1.kategoriyasi} , {t1.narxi}")
+# print(f"{t2.nomi} , {t2.kategoriyasi} , {t2.narxi}")
+# print(f"{t3.nomi} , {t3.kategoriyasi} , {t3.narxi}")
+# print(f"{t4.nomi} , {t4.kategoriyasi} , {t4.narxi}")
+# print(f"{t5.nomi} , {t5.kategoriyasi} , {t5.narxi}")
+
+# # 14-masala
+# class Kurs:
+#     def __init__(self, nomi, davomiyligi, talabalar_soni):
+#         self.nomi = nomi
+#         self.davomiyligi = davomiyligi
+#         self.talabalar_soni = talabalar_soni
+#
+# k1 = Kurs("Python asoslari", 40, 150)
+# k2 = Kurs("JavaScript", 35, 120)
+# k3 = Kurs("Data Science", 60, 80)
+# k4 = Kurs("Web Development", 50, 200)
+# k5 = Kurs("Mobile Apps", 45, 95)
+# print("_"*20)
+# print(f"{k1.nomi} , {k1.davomiyligi} , {k1.talabalar_soni}")
+# print(f"{k2.nomi} , {k2.davomiyligi} , {k2.talabalar_soni}")
+# print(f"{k3.nomi} , {k3.davomiyligi} , {k3.talabalar_soni}")
+# print(f"{k4.nomi} , {k4.davomiyligi} , {k4.talabalar_soni}")
+# print(f"{k5.nomi} , {k5.davomiyligi} , {k5.talabalar_soni}")
+
+# # 15-masala
+# class Xarid:
+#     def __init__(self, nomi, miqdori, narxi):
+#         self.nomi = nomi
+#         self.miqdori = miqdori
+#         self.narxi = narxi
+#
+# x1 = Xarid("Non", 2, 5000)
+# x2 = Xarid("Sut", 3, 12000)
+# x3 = Xarid("Go'sht", 1, 80000)
+# x4 = Xarid("Sabzavot", 2, 15000)
+# x5 = Xarid("Meva", 1, 25000)
+# print("_"*20)
+# print(f"{x1.nomi} , {x1.miqdori} , {x1.narxi}")
+# print(f"{x2.nomi} , {x2.miqdori} , {x2.narxi}")
+# print(f"{x3.nomi} , {x3.miqdori} , {x3.narxi}")
+# print(f"{x4.nomi} , {x4.miqdori} , {x4.narxi}")
+# print(f"{x5.nomi} , {x5.miqdori} , {x5.narxi}")
